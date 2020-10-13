682c065622c28b3146a705a458d9d1311cf58497
wangguan <geekwang1024@qq.com>
2020-10-13 17:04:35 +0800

    为conn中的feign服务增加熔断机制

--- database-manager/src/main/java/com/zyv1/databasemanager/DatabaseManagerApplication.java
+++ database-manager/src/main/java/com/zyv1/databasemanager/DatabaseManagerApplication.java
@@ -3,11 +3,9 @@ package com.zyv1.databasemanager;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @MapperScan("com.zyv1.databasemanager.dao")
-@EnableFeignClients
 public class DatabaseManagerApplication {
 
 	public static void main(String[] args) {
